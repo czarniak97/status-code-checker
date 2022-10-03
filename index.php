62ce46cb6c7ae04119a7cff218710b1ca2857460
czarniak97 <51341140+czarniak97@users.noreply.github.com>
2022-10-03 21:53:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,13 @@ include 'templates/header.html';
 include 'templates/content.html';
 
 if (isset($_POST['pagesToCheck'])) {
-    $curl = new Curl();
+    
     
     $pagesArray = explode(PHP_EOL, $_POST['pagesToCheck']);
 
     echo '<table>';
     foreach ($pagesArray as $page) {
+        $curl = new Curl();
         $delimiter = 0;
         echo '<tr>';
         pageCheck($curl, $page, $delimiter);
