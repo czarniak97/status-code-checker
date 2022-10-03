b62d2930a71933cec9072139dbad1492d71b6672
czarniak97 <51341140+czarniak97@users.noreply.github.com>
2022-10-03 22:35:58 +0200

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -19,23 +19,23 @@ function pageCheck($curl, $page, $delimiter)
         pageCheck($curl, $curl->info['redirect_url'], $delimiter);
     }
 }*/
-function pageCheck($curl, $page, $delimiter)
+function pageCheck($page, $delimiter)
 {
     $curl = curl_init();
     curl_setopt_array($curl, [CURLOPT_URL => $page, CURLOPT_HEADER => true, CURLOPT_NOBODY => true, CURLOPT_RETURNTRANSFER => 1, CURLOPT_SSL_VERIFYPEER => false]);
     $info = curl_exec($curl);
     
-    $columnColor = getColumnColor(info['http_code']);
+    $columnColor = getColumnColor($info['http_code']);
 
-    echo '<td class="' . $columnColor . '" >' . info['url'] . '</td>';
-    echo '<td class="' . $columnColor . '" >' . info['http_code'] . '</td>';
+    echo '<td class="' . $columnColor . '" >' . $info['url'] . '</td>';
+    echo '<td class="' . $columnColor . '" >' . $info['http_code'] . '</td>';
 
     if ($delimiter > 4)
         echo '<td class="loopWarning" >Prawdopodobna pętla przekierowań</td>';
 
-    if (info['http_code'] >= 300 && info['http_code'] < 400) {
+    if ($info['http_code'] >= 300 && $info['http_code'] < 400) {
         $delimiter++;
-        pageCheck($curl, info['redirect_url'], $delimiter);
+        pageCheck($curl, $info['redirect_url'], $delimiter);
     }
     curl_close($curl);
 }
