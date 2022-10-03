a1f7e46ee0b9cae3ba529398a904d363b27b9d67
czarniak97 <51341140+czarniak97@users.noreply.github.com>
2022-10-03 21:31:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1); 
-error_reporting(E_ALL);
-
 include 'classes/Curl.php';
 include 'includes/functions.php';
 include 'templates/header.html';
@@ -10,7 +6,7 @@ include 'templates/content.html';
 
 if (isset($_POST['pagesToCheck'])) {
     $curl = new Curl();
-    echo $curl;
+    
     $pagesArray = explode(PHP_EOL, $_POST['pagesToCheck']);
 
     echo '<table>';
